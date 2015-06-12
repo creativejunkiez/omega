e6be5bbe6b216fa8a7d9b6775451b8bf80189a99
Kumar Anirudha <anirudhastark@yahoo.com>
2015-06-12 16:07:28 +0530

    main page modified

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,10 @@
 
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
 
-	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
-	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
 	<script src="js/modernizr.js"></script> <!-- Modernizr -->
   	
 	<title><?= $app_name ?> | Welcome</title>
@@ -38,18 +40,33 @@
 
 		<span class="cd-marker color-1"></span>	
 	</nav> <!-- .cd-3d-nav-container -->
-	
-	<header class="cd-header">
-		<a href="#0" class="cd-logo"><img class="cjlogo" src="img/cjlogo.png" alt="Logo"></a>
-		<a href="#0" class="cd-3d-nav-trigger">
-			Menu
-			<span></span>
-		</a>
+
+	<header class="cd-header col-md-12">
+		<div class="col-md-3">
+			<a href="#0" class="cd-logo"><img class="cjlogo" src="img/cjlogo.png" alt="Logo"></a>
+		</div>
+		<div class="col-md-9">
+			<a href="#0" class="cd-3d-nav-trigger">Menu<span></span></a>
+		</div>
 	</header> <!-- .cd-header -->
 	
 	<main>
-		<h1><?= $app_name ?></h1>
-		<!-- all your content here -->
+		<div class="container col-md-12">
+			<div class="category_bar">
+				Category bar
+			</div>
+			<div class="col-md-9 col-sm-12">
+				<div class="col-sm-8"><h2>Post Title</h2></div>
+				<div class="col-sm-4"><h3>Post Author</h3></div>
+				<img src="uploads/post_one.jpg">
+			</div>
+			<div class="col-md-3">
+				Sidebar
+			</div>
+			<div class="footer">
+				Powered by Creative Junkiez
+			</div>
+		</div>
 	</main>
 	
 	
