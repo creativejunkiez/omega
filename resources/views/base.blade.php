6edeb29d9d274ef47e9285b930815e344e4dcf48
Kumar Anirudha <anirudhastark@yahoo.com>
2015-06-13 11:31:20 +0530

    db files removed

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -504,16 +504,11 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 										</div>
 										</div>
 									</div>
-									<!-- banner-bottom-right -->
 									<div class="clearfix"> </div>
-									
 								</div>
-								<!-- //banner-bottom-grids -->
 							</div>
 						</div>
 					</div>
-					<!-- //banner -->
-				
 			</div>
 		 </div>
 	</div>
@@ -530,55 +525,52 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<div class="up-arrow-right">
 			<a class="scroll" href="#home">Back to Top</a>
 		</div>
-			<div class="clearfix"> </div>
-	</div>	
-	<!-- //bg-banner -->
+		<div class="clearfix"> </div>
+	</div>
 	<div class="footer">
-		<!-- container -->
-			<div class="footer-grids">
-				
-				<div class="col-md-4 viewport">
-					<h3>About Creative Junkiez</h3>
-					<p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.
-						<span>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions will uncover many web sites still in their infancy have evolved.</span> 
-						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form model text, and.
-					</p>
-				</div>
-				<div class="col-md-4 recent-posts footer-left">
-					<h3>Latest Posts</h3>
-					<ul>
-						<li><a href="#">Skype 5.0 Beta for Mac Arrives… But Where’s Facebook?</a></li>
-						<li><a href="#">Apple Fails to Fix iPhone Daylight Savings Time Alarm Bug</a></li>
-						<li><a href="#">71% of Internet Users Run Latest Version of Their Browsers</a></li>
-						<li><a href="#">Netgear Wants to Wirelessly Connect New Televisions</a></li>
-						<li><a href="#">With Homegrown Talent, a Rising Power on Long Island</a></li>
-						<li><a href="#">Displaying Shooting Touch, Knicks Return to Prime Time</a></li>
-					</ul>
-				</div>
-				
-				<div class="col-md-4 footer-flick">
-					<h3>Something</h3>
-					<!--<div class="fb-page" data-href="https://www.facebook.com/creativejunkiez" data-hide-cover="true" data-show-facepile="true" data-show-posts="false">
-						<div class="fb-xfbml-parse-ignore">
-							<blockquote cite="https://www.facebook.com/facebook">
-								<a href="https://www.facebook.com/creativejunkiez">Facebook</a>
-							</blockquote>
-						</div>
-					</div>
-					<div id="fb-root"></div>
-						<script>(function(d, s, id) {
-						  var js, fjs = d.getElementsByTagName(s)[0];
-						  if (d.getElementById(id)) return;
-						  js = d.createElement(s); js.id = id;
-						  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
-						  fjs.parentNode.insertBefore(js, fjs);
-						}(document, 'script', 'facebook-jssdk'));</script>-->
-				</div>
-				<div class="clearfix"> </div>
-				<div class="copyright">
-					<p>Copyrights © 2015 <?= $app_name ?> All rights reserved </p>
-				</div>
+		<div class="footer-grids">
+			<div class="col-md-4 viewport">
+				<h3>About Creative Junkiez</h3>
+				<p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.
+					<span>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions will uncover many web sites still in their infancy have evolved.</span> 
+					There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form model text, and.
+				</p>
 			</div>
+			<div class="col-md-4 recent-posts footer-left">
+				<h3>Latest Posts</h3>
+				<ul>
+					<li><a href="#">Skype 5.0 Beta for Mac Arrives… But Where’s Facebook?</a></li>
+					<li><a href="#">Apple Fails to Fix iPhone Daylight Savings Time Alarm Bug</a></li>
+					<li><a href="#">71% of Internet Users Run Latest Version of Their Browsers</a></li>
+					<li><a href="#">Netgear Wants to Wirelessly Connect New Televisions</a></li>
+					<li><a href="#">With Homegrown Talent, a Rising Power on Long Island</a></li>
+					<li><a href="#">Displaying Shooting Touch, Knicks Return to Prime Time</a></li>
+				</ul>
+			</div>
+			
+			<div class="col-md-4 footer-flick">
+				<h3>Something</h3>
+				<!--<div class="fb-page" data-href="https://www.facebook.com/creativejunkiez" data-hide-cover="true" data-show-facepile="true" data-show-posts="false">
+					<div class="fb-xfbml-parse-ignore">
+						<blockquote cite="https://www.facebook.com/facebook">
+							<a href="https://www.facebook.com/creativejunkiez">Facebook</a>
+						</blockquote>
+					</div>
+				</div>
+				<div id="fb-root"></div>
+					<script>(function(d, s, id) {
+					  var js, fjs = d.getElementsByTagName(s)[0];
+					  if (d.getElementById(id)) return;
+					  js = d.createElement(s); js.id = id;
+					  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+					  fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>-->
+			</div>
+			<div class="clearfix"> </div>
+			<div class="copyright">
+				<p>Copyrights © 2015 <?= $app_name ?> All rights reserved </p>
+			</div>
+		</div>
 	</div>
 	</div>
 </body>
